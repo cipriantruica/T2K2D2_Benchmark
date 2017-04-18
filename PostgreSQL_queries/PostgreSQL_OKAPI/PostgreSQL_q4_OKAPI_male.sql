e08245fd0219d802c60079294dcabc3e7aa48e31
ciprian.truica <ciprian.octavian.truica@gmail.com>
2017-04-18 15:50:23 +0300

    T2K2 benchmark

---
+++ PostgreSQL_queries/PostgreSQL_OKAPI/PostgreSQL_q4_OKAPI_male.sql
@@ -0,0 +1,92 @@
+\set startDate '''2015-09-17 00:00:00'''
+\set endDate '''2015-09-18 00:00:00'''
+\set xStart 20
+\set xEnd 40
+\set yStart -100
+\set yEnd 100
+\set gender '''male'''
+\set k1 1.6
+\set b 0.75
+\set top 10
+
+with 
+    q_docLen as (select d.id id, sum(v.count) docLen
+                from documents d
+                    inner join vocabulary v
+                    on v.id_document = d.id
+                    inner join documents_authors da
+                        inner join authors a
+                            inner join genders g
+                            on a.id_gender = g.id
+                        on da.id_author = a.id
+                    on d.id = da.id_document
+                    inner join geo_location gl
+                    on d.id_geo_loc = gl.id 
+                where g.type = :gender
+                    and gl.X between :xStart and :xEnd
+                    and gl.Y between :yStart and :yEnd
+                    and d.document_date between :startDate and :endDate
+                group by d.id),
+    q_wordCountDocs as (select v.id_word id_word, count(distinct v.id_document) wordCountDocs
+                        from vocabulary v 
+                            where v.id_document in (select d.id
+                                                    from documents d
+                                                    inner join documents_authors da
+                                                        inner join authors a
+                                                            inner join genders g
+                                                            on a.id_gender = g.id
+                                                        on da.id_author = a.id
+                                                    on d.id = da.id_document
+                                                    inner join geo_location gl
+                                                        on d.id_geo_loc = gl.id 
+                                                    where g.type = :gender
+                                                        and gl.X between :xStart and :xEnd
+                                                        and gl.Y between :yStart and :yEnd
+                                                        and d.document_date between :startDate and :endDate)
+                            group by v.id_word),
+    q_noDocs as (select d.id id
+                    from documents d
+                        inner join documents_authors da
+                            inner join authors a
+                                inner join genders g
+                                on a.id_gender = g.id
+                            on da.id_author = a.id
+                        on d.id = da.id_document
+                        inner join geo_location gl
+                        on d.id_geo_loc = gl.id 
+                    where g.type = :gender
+                        and gl.X between :xStart and :xEnd
+                        and gl.Y between :yStart and :yEnd
+                        and d.document_date between :startDate and :endDate)
+    select q2.word word, sum(q2.okapi) sokapi 
+        from
+            (select d.id id, w.word word, -- v.id_word, v.tf, q_dl.docLen, q_wcd.wordCountDocs,
+                    (v.tf * (1 + ln((select count(id) from q_noDocs)::float/q_wcd.wordCountDocs)) * (:k1 + 1))/
+                    (v.tf + :k1 * (1 - :b + :b * q_dl.docLen::float / (select avg(docLen) from q_docLen))) okapi
+            from documents d
+                inner join vocabulary v
+                    inner join words w 
+                    on w.id = v.id_word
+                on v.id_document = d.id
+                inner join documents_authors da
+                    inner join authors a
+                        inner join genders g
+                        on a.id_gender = g.id
+                    on da.id_author = a.id
+                on d.id = da.id_document
+                inner join geo_location gl
+                on d.id_geo_loc = gl.id 
+                inner join q_docLen q_dl
+                on q_dl.id = d.id
+                inner join q_wordCountDocs q_wcd
+                on q_wcd.id_word =  v.id_word
+            where g.type = :gender
+                and gl.X between :xStart and :xEnd
+                and gl.Y between :yStart and :yEnd
+                and d.document_date between :startDate and :endDate) q2
+        group by q2.word
+        order by 2 desc
+        limit :top;
+
+\q
+
---
+++ README.md
@@ -0,0 +1,1 @@
+T2K2:  The Twitter Top-K Keywords Benchmark
